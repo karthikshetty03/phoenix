768acf9f952f582525fa5a9e6fdede1b848ffde1
Karthik Shetty <f20180141@hyderabad.bits-pilani.ac.in>
2021-03-10 13:04:56 +0530

    style(app): made collections info panel compact for mobile/msmaller views

--- packages/phoenix-ng/projects/phoenix-ui-components/src/components/ui-menu/collections-info/collections-info-overlay/collections-info-overlay.component.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/src/components/ui-menu/collections-info/collections-info-overlay/collections-info-overlay.component.ts
@@ -3,58 +3,72 @@ import { PrettySymbols } from 'phoenix-event-display';
 import { EventDisplayService } from '../../../../services/event-display.service';
 
 @Component({
-  selector: 'app-collections-info-overlay',
-  templateUrl: './collections-info-overlay.component.html',
-  styleUrls: ['./collections-info-overlay.component.scss']
+	selector: 'app-collections-info-overlay',
+	templateUrl: './collections-info-overlay.component.html',
+	styleUrls: [ './collections-info-overlay.component.scss' ]
 })
 export class CollectionsInfoOverlayComponent implements OnInit {
+	@Input() showObjectsInfo: boolean;
+	collections: string[];
+	selectedCollection: string;
+	showingCollection: any;
+	collectionColumns: string[];
+	activeObject: any;
+	isSmallScreen: boolean;
 
-  @Input() showObjectsInfo: boolean;
-  collections: string[];
-  selectedCollection: string;
-  showingCollection: any;
-  collectionColumns: string[];
-  activeObject: any;
+	constructor(private elementRef: ElementRef, private eventDisplay: EventDisplayService) {}
 
-  constructor(private elementRef: ElementRef, private eventDisplay: EventDisplayService) { }
+	ngOnInit() {
+		/*event listener if screen is resized or check if smallScreen or not*/
+		let screenSize = window.matchMedia('(max-width: 600px)');
+		screenSize.matches == true ? (this.isSmallScreen = true) : (this.isSmallScreen = false);
+		screenSize.addEventListener('change', () => {
+			if (screenSize.matches == true) {
+				this.isSmallScreen = true;
+			} else {
+				this.isSmallScreen = false;
+			}
+		});
 
-  ngOnInit() {
-    this.eventDisplay.listenToDisplayedEventChange((event) => this.collections = this.eventDisplay.getCollections());
-    this.activeObject = this.eventDisplay.getActiveObjectId();
-    this.activeObject.onUpdate((value: string) => {
-      if (document.getElementById(value)) {
-        document.getElementById(value).scrollIntoView(false);
-      }
-    });
-  }
+		this.eventDisplay.listenToDisplayedEventChange(
+			(event) => (this.collections = this.eventDisplay.getCollections())
+		);
 
-  changeCollection(selected: any) {
-    const value = selected.target.value;
-    this.selectedCollection = value;
-    this.showingCollection = this.eventDisplay.getCollection(value).map(PrettySymbols.getPrettyParams);
-    this.collectionColumns = Object.keys(this.showingCollection[0]).filter((column) => column !== 'uuid');
-  }
+		this.activeObject = this.eventDisplay.getActiveObjectId();
 
-  lookAtObject(uuid: string) {
-    if (uuid) {
-      this.activeObject.update(uuid);
-      this.eventDisplay.lookAtObject(uuid);
-    }
-  }
+		this.activeObject.onUpdate((value: string) => {
+			if (document.getElementById(value)) {
+				document.getElementById(value).scrollIntoView(false);
+			}
+		});
+	}
 
-  highlightObject(uuid: string) {
-    if (uuid) {
-      this.activeObject.update(uuid);
-      this.eventDisplay.highlightObject(uuid);
-    }
-  }
+	changeCollection(selected: any) {
+		const value = selected.target.value;
+		this.selectedCollection = value;
+		this.showingCollection = this.eventDisplay.getCollection(value).map(PrettySymbols.getPrettyParams);
+		this.collectionColumns = Object.keys(this.showingCollection[0]).filter((column) => column !== 'uuid');
+	}
 
-  addLabel(index: number, uuid: string) {
-    const labelValue = this.elementRef.nativeElement.querySelector(`#label${index}`).value;
-    if (this.selectedCollection) {
-      // Empty labelValue will remove the label object
-      this.eventDisplay.addLabelToObject(labelValue, this.selectedCollection, index, uuid);
-    }
-  }
+	lookAtObject(uuid: string) {
+		if (uuid) {
+			this.activeObject.update(uuid);
+			this.eventDisplay.lookAtObject(uuid);
+		}
+	}
 
+	highlightObject(uuid: string) {
+		if (uuid) {
+			this.activeObject.update(uuid);
+			this.eventDisplay.highlightObject(uuid);
+		}
+	}
+
+	addLabel(index: number, uuid: string) {
+		const labelValue = this.elementRef.nativeElement.querySelector(`#label${index}`).value;
+		if (this.selectedCollection) {
+			// Empty labelValue will remove the label object
+			this.eventDisplay.addLabelToObject(labelValue, this.selectedCollection, index, uuid);
+		}
+	}
 }
--- packages/phoenix-ng/projects/phoenix-ui-components/src/components/ui-menu/collections-info/collections-info.component.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/src/components/ui-menu/collections-info/collections-info.component.ts
@@ -4,30 +4,29 @@ import { ComponentPortal } from '@angular/cdk/portal';
 import { CollectionsInfoOverlayComponent } from './collections-info-overlay/collections-info-overlay.component';
 
 @Component({
-  selector: 'app-collections-info',
-  templateUrl: './collections-info.component.html',
-  styleUrls: ['./collections-info.component.scss']
+	selector: 'app-collections-info',
+	templateUrl: './collections-info.component.html',
+	styleUrls: [ './collections-info.component.scss' ]
 })
 export class CollectionsInfoComponent implements OnInit, OnDestroy {
+	showObjectsInfo = false;
+	overlayWindow: ComponentRef<CollectionsInfoOverlayComponent>;
 
-  showObjectsInfo = false;
-  overlayWindow: ComponentRef<CollectionsInfoOverlayComponent>;
+	constructor(private overlay: Overlay) {}
 
-  constructor(private overlay: Overlay) { }
+	ngOnInit() {
+		const overlayRef = this.overlay.create();
+		const overlayPortal = new ComponentPortal(CollectionsInfoOverlayComponent);
+		this.overlayWindow = overlayRef.attach(overlayPortal);
+		this.overlayWindow.instance.showObjectsInfo = this.showObjectsInfo;
+	}
 
-  ngOnInit() {
-    const overlayRef = this.overlay.create();
-    const overlayPortal = new ComponentPortal(CollectionsInfoOverlayComponent);
-    this.overlayWindow = overlayRef.attach(overlayPortal);
-    this.overlayWindow.instance.showObjectsInfo = this.showObjectsInfo;
-  }
+	ngOnDestroy(): void {
+		this.overlayWindow.destroy();
+	}
 
-  ngOnDestroy(): void {
-    this.overlayWindow.destroy();
-  }
-
-  toggleOverlay() {
-    this.showObjectsInfo = !this.showObjectsInfo;
-    this.overlayWindow.instance.showObjectsInfo = this.showObjectsInfo;
-  }
+	toggleOverlay() {
+		this.showObjectsInfo = !this.showObjectsInfo;
+		this.overlayWindow.instance.showObjectsInfo = this.showObjectsInfo;
+	}
 }
